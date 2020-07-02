4fcd6d12f730d402821e7f747dfe73fe8c42e01a
Aakash Kumar Goryan <aakashgoryan@gmail.com>
2020-07-02 10:21:54 +0530

    First commit

---
+++ config/passport.js
@@ -0,0 +1,79 @@
+// let passport = require('passport');
+// let Schema = require('../database/schema');
+// let LocalStrategy = require('passport-local').Strategy;
+// var bcrypt = require('bcrypt');
+// const saltRounds = 10;
+
+// passport.serializeUser(function (user,done) {
+//     console.log("Sserialize");
+//     console.log(user.Type);
+//     console.log(user.id);
+//     done(null,{
+//         id: user.id,
+//         type: user.Type
+//     });
+// });
+
+// passport.deserializeUser(function (user,done) {
+
+//     if(user.type==='Startup')
+//     {
+//         console.log("startupdeserialize");
+//         Schema.startup.findById(user.id,function (err,user) {
+//             console.log("start");
+//             done(err,user);
+//         });
+//     }
+//     else if (user.type==='Investor'){
+//         console.log("investordeserialize");
+//         Schema.investor.findById(user.id,function (err,user) {
+//             console.log("start");
+//             done(err,user);
+//         });
+//     }
+// });
+
+// passport.use('local.Startup',new LocalStrategy({
+//     usernameField: 'Email',
+//     passwordField: 'Password',
+// },function (username,password,done) {
+//     console.log("pass " + username);
+//     console.log("pass " + password);
+//     Schema.startup.findOne({'Email': username}, function (err, user) {
+//         if (err) {
+//             return done(err);
+//         }
+//         if(user==null) {
+//             console.log("StaMail");
+//             return done(null,false,{ message: 'Incorrect Email' })
+//         }
+
+//         if (bcrypt.compareSync(user.Password,password)) {
+//             console.log("Stapass");
+//             return done(null, false, {message: 'Wrong password'})
+//         }
+//         return done(null, user);
+//     });
+// }));
+
+// passport.use('local.Investor',new LocalStrategy({
+//     usernameField: 'Email',
+//     passwordField: 'Password',
+// },function (username,password,done) {
+//     console.log("pass " + username);
+//     console.log("pass " + password);
+//     Schema.investor.findOne({'Email': username}, function (err, user) {
+//         if (err) {
+//             return done(err);
+//         }
+//         if(user==null) {
+//             console.log("StaMail");
+//             return done(null,false,{ message: 'Incorrect Email' })
+//         }
+//         if (bcrypt.compareSync(user.Password,password)) {
+//             console.log("Stapass");
+//             return done(null, false, {message: 'Wrong password'})
+//         }
+//         return done(null, user);
+//     });
+// }));
---
+++ database/connect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv/config');
+///const databaseName = 'moneygustdb';
+///const url = 'mongodb://localhost/'+databaseName;
+///mLab Database
+const url = process.env.MONGOURI;
+
+function connect() {
+    mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true})
+        .then(function (db) {
+            console.log("database port: " + db['connections'][0]['port'])
+        });
+    let db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        console.log('Connected to database')
+    });
+}
+
+module.exports = connect;
