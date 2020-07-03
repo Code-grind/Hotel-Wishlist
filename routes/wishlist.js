0a739351ce03f1455d0f340b81f001b0dad7684a
Aakash Kumar Goryan <aakashgoryan@gmail.com>
2020-07-03 12:30:21 +0530

    Delete route changed

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,13 +2,13 @@ let express = require('express');
 let router = express.Router();
 let Schema = require('../database/schema');
 
-router.get('/',function (req,res) {
-    if(req.user) {
+router.get('/', function (req, res) {
+    if (req.user) {
         console.log('Get Wishlist')
         let itemsProcessed = 0;
         let wishListHotel = [];
         req.user.Wishlist.forEach(function (value, index, array) {
-            Schema.hotel.findOne({_id: value}).lean().exec(function (err, msg) {
+            Schema.hotel.findOne({ _id: value }).lean().exec(function (err, msg) {
                 if (err) return done(err);
                 if (msg === null) res.sendStatus(404);
                 wishListHotel.push(msg);
@@ -28,7 +28,7 @@ router.get('/',function (req,res) {
 });
 
 /**Route to store user Wishlist*/
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     console.log('Here');
     let wishedhotel = [];
     req.body.wishlist.forEach(hotel => {
@@ -45,7 +45,7 @@ router.post('/', function(req, res) {
     wishedhotel.forEach(hotel => {
         hotel.save(function (err, data) {
             if (err) throw err;
-            Schema.user.findOne({_id: req.user.id}, function (err, user) {
+            Schema.user.findOne({ _id: req.user.id }, function (err, user) {
                 if (err) {
                     return done(err);
                 }
@@ -60,61 +60,31 @@ router.post('/', function(req, res) {
             });
         });
     });
-    
+
     res.send("done");
 });
 
 /**Route to Delete a notification*/
-router.delete('/', function (req,res) {
-    console.log('Here');
-    let wishedhotel = [];
-    req.body.wishlist.forEach(hotel => {
-        // console.log(hotel);
-        wishedhotel.push(Schema.hotel({
-            RecevID: req.user.id,
-            Name: hotel.Name,
-            Price: hotel.Price[0],
-            Address: hotel.Address[0],
-            Rating: hotel.Rating[0]
-        }));
-    });
-
-    wishedhotel.forEach(hotel => {
-        hotel.save(function (err, data) {
-            if (err) throw err;
-            Schema.user.findOne({_id: req.user.id}, function (err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (user === null) {
-                    res.sendStatus(404);
-                }
-                user.Wishlist.push(data._id);
-                user.save(function (err, data) {
-                    if (err) throw err;
-                });
-                console.log(data)
-            });
-        });
-    });
-    
-    res.send("done");
-    console.log(req.params.hotel_id);
-    Schema.hotel.findById( req.params.hotel_id, function (err, recipient) {
-        if (err)
-            return done(err);
-        Schema.user.updateOne({_id: recipient.RecevID}, { 
-            $pull: { 'Notification': req.params.notification_id } },function (err) {
-            if(err)
-                return done(err);
-            console.log('User Updated');
-            Schema.hotel.deleteOne({_id: req.params.hotel_id},function (err) {
-                if(err)
-                    return done(err);
-                res.send('Deletion done');
-            });
-        })
-    });
+router.delete('/', function (req, res) {
+    console.log('DELETE')
+    console.log(req.body)
+    res.send('done');
+    // Schema.hotel.findById(req.body.id, function (err, recipient) {
+    //     if (err)
+    //         return done(err);
+    //     Schema.user.updateOne({ _id: recipient.RecevID }, {
+    //         $pull: { 'Wishlist': id }
+    //     }, function (err) {
+    //         if (err)
+    //             return done(err);
+    //         console.log('User Updated');
+    //         Schema.hotel.deleteOne({ _id: id }, function (err) {
+    //             if (err)
+    //                 return done(err);
+    //             res.send('Deletion done');
+    //         });
+    //     })
+    // });
 });
 
 module.exports = router;
